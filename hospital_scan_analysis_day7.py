9031afc6f41cdd6787276723bf02e54c4ae5ea31
SWATI SAHU <swatisahu18017@gmail.com>
2026-03-09 20:32:25 +0530

    Add files via upload

---
+++ hospital_scan_analysis_day7.py
@@ -0,0 +1,10 @@
+scans = ["MRI","CT","X-RAY","MRI","ULTRASOUND","CT","X-RAY","MAMMOGRAPHY"]
+print("TOTAL scans: ",len(scans))
+print("MRI scans:",scans.count("MRI"))
+print("CT scans:",scans.count("CT"))
+print("X-RAY scans:",
+      scans.count("X-RAY"))
+print("ULTRASOUND scans:",
+      scans.count("ULTRASOUND"))
+print("MAMMOGRAPHY scans:",
+      scans.count("MAMMOGRAPHY"))
